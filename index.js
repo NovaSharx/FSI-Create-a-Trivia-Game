ee01c7d29d889f1057939303f5c4efbd9afc41d3
Davin <davin-dj@hotmail.com>
2022-07-01 12:14:47 -0400

    Can ask questions and check if correct.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your Code Here
+
+userName = window.prompt('Please enter your name:')
+
+
+for (i = 0; i < questions.length; i++) {
+    let question = questions[i]
+    userAnswer = window.prompt(question.text)
+    if (userAnswer === question.correctAnswer) {
+        console.log('correct!')
+    }
+}
