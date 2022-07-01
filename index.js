0e7333744d53113f50a359f6b3a10d4ff4fe84f2
Davin <davin-dj@hotmail.com>
2022-07-01 12:43:43 -0400

    Can display the user score and prompt for a retry.

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // Your Code Here
 
-userName = window.prompt('Please enter your name:')
+function runTriviaGame() {
+    let userName = window.prompt('Please enter your name:')
+    let userScore = 0
 
+    for (i = 0; i < questions.length; i++) {
+        let question = questions[i]
+        userAnswer = window.prompt(question.text)
+        if (userAnswer === question.correctAnswer) {
+            userScore += 10
+        }
+    }
 
-for (i = 0; i < questions.length; i++) {
-    let question = questions[i]
-    userAnswer = window.prompt(question.text)
-    if (userAnswer === question.correctAnswer) {
-        console.log('correct!')
+    window.alert('Your score is: '+ userScore)
+    promptRetry()
+}
+
+function promptRetry() {
+    let retry = window.prompt('Would you like to continue the game? (Y/N):')
+    if (retry === 'Y') {
+        runTriviaGame()
+    }
+    else if (retry === 'N') {
+        window.alert('Game has ended.')
+    }
+    else {
+        window.alert('That was not a valid response.')
+        promptRetry()
     }
 }
+
+runTriviaGame()
+
